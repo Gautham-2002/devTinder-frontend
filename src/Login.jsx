39ed135827d3c27006457b531f32892f2f3add91
gautham <gautham.gokulakonda@apxor.com>
2025-01-21 01:00:35 +0530

    bypass cors and set cookies in dev mode

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,66 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("gautham@gokulakonda.com");
+  const [password, setPassword] = useState("Gautham@2707");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true, // this is needed to set the cookie and send it to the backend in subsequent requests
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center mt-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
