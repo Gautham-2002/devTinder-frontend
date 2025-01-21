7aa00fca33f477e3f181457e3b505f54418f5bac
gautham <gautham.gokulakonda@apxor.com>
2025-01-21 22:48:20 +0530

    redirect to login if not authenticated

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,8 +1,35 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  useEffect(() => {
+    if (!userData) {
+      (async () => {
+        try {
+          const res = await axios.get(BASE_URL + "/profile/view", {
+            withCredentials: true,
+          });
+
+          dispatch(addUser(res.data));
+        } catch (error) {
+          navigate("/login");
+          console.log(error);
+        }
+      })();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <NavBar />
